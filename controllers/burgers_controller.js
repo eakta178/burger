fa2a3336fe7bccc45e31004ec2ab32dfd60505b6
eakta178 <eakta.pandey@gmail.com>
2018-02-19 12:09:38 -0800

    working code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,36 +4,24 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
-    var hbsObject = {
-      cats: data
-    };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+  burger.all(function(burger_data) {
+
+    console.log(burger_data);
+    res.render("index", {burger_data});
   });
 });
-router.post("/api/burgers", function(req, res) {
-  burger.create(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+router.post("/burgers/create", function(req, res) {
+  burger.create(req.body.burger_name, function(result) {
+    console.log(result);
+    res.redirect('/');
   });
 });
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-  console.log("condition", condition);
-  burger.update(
-    {
-      devoured: req.body.devoured
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
-    }
-  );
+router.put("/burgers/update", function(req, res) {
+  burger.update(req.body.burger_id, function(result){
+    console.log(result);
+    res.redirect('/')
+  })
+  
 });
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
-var PORT = process.env.PORT || 3000;
+var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+var PORT = process.env.PORT || 3300;
 
 var app = express();
 
@@ -13,9 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use(methodOverride('_method'));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
